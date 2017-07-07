b520fca9bfdcb3ca4d627fb0f9a52d566bd7b3a8
samlanh <mok_channy@yahoo.com>
2017-07-07 10:06:54 +0700

    commit permission link

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -50,54 +50,62 @@ class IndexController extends Zend_Controller_Action
 					$user_id=$db_user->getUserID($user_name);
 					$user_info = $db_user->getUserInfo($user_id);
 					$arr_acl=$db_user->getArrAcl($user_info['user_type']);
-					
-					$session_user->user_id=$user_id;
-					$session_user->user_name=$user_name;
-					$session_user->pwd=$password;		
-					$session_user->level= $user_info['user_type'];
-					$session_user->last_name= $user_info['last_name'];
-					$session_user->first_name= $user_info['first_name'];
-					$session_user->branch_id= $user_info['branch_id'];
-					
 					$a_i = 0;
 					$arr_actin = array();
 					$arr_module=array();
 					for($i=0; $i<count($arr_acl);$i++){
 						$arr_module[$i]=$arr_acl[$i]['module'];
-						if($arr_acl[$i]['module'] == 'exchange'){
-							if($arr_acl[$i]['action'] == "index" || $arr_acl[$i]['action'] == "add" || $arr_acl[$i]['action'] == "edited" ) {
-								continue;
-							}
-							$arr_actin[$a_i++] = $arr_acl[$i]['action'];
-						}
-					}					
-					
+					}
+					$arr_module=(array_unique($arr_module));
+					$arr_actin=(array_unique($arr_actin));
 					$arr_module=$this->sortMenu($arr_module);
-					
+						
 					$session_user->arr_acl = $arr_acl;
 					$session_user->arr_module = $arr_module;
 					$session_user->arr_actin = $arr_actin;
+					
+					$session_user->user_id=$user_id;
+					$session_user->user_name=$user_name;
+					$session_user->pwd=$password;
+					$session_user->level= $user_info['user_type'];
+					$session_user->last_name= $user_info['last_name'];
+					$session_user->first_name= $user_info['first_name'];
+					$session_user->branch_id= $user_info['branch_id'];
+					
+// 					for($i=0; $i<count($arr_acl);$i++){
+// 						$arr_module[$i]=$arr_acl[$i]['module'];
+// 						if($arr_acl[$i]['module'] == 'exchange'){
+// 							if($arr_acl[$i]['action'] == "index" || $arr_acl[$i]['action'] == "add" || $arr_acl[$i]['action'] == "edited" ) {
+// 								continue;
+// 							}
+// 							$arr_actin[$a_i++] = $arr_acl[$i]['action'];
+// 						}
+// 					}					
+// 					$arr_module=$this->sortMenu($arr_module);
+					
+// 					$session_user->arr_acl = $arr_acl;
+// 					$session_user->arr_module = $arr_module;
+// 					$session_user->arr_actin = $arr_actin;
 						
 					$session_user->lock();
 					
 					$log=new Application_Model_DbTable_DbUserLog();
 					$log->insertLogin($user_id);
 					foreach ($arr_module AS $i => $d){
-						if($d !== 'user'){
-							$url = '/' . $arr_module[2];
-						}
-						else{
+// 						if($d !== 'user'){
+// 							$url = '/' . $arr_module[2];
+// 						}
+// 						else{
 							$url = self::REDIRECT_URL;
 							break;
-						}
+// 						}
 					}	
 					Application_Form_FrmMessage::redirectUrl("/home");	
 					exit();										
 				}
 				else{					
 					$this->view->msg = 'ឈ្មោះ​អ្នក​ប្រើ​ប្រាស់ និង ពាក្យ​​សំងាត់ មិន​ត្រឺម​ត្រូវ​ទេ ';
-				}
-					
+				}	
 			}
 			else{				
 				$this->view->msg = 'លោកអ្នកមិនមានសិទ្ធិប្រើប្រាស់ទេ!';
@@ -106,8 +114,7 @@ class IndexController extends Zend_Controller_Action
     }
     
     protected function sortMenu($menus){
-    	$menus_order = Array ( 'setting','rsvAcl','user','subagent','agent','transfer','exchange','reports');
-//     	$menus_order = Array ( 'rsvAcl','user','subagent','agent','transfer','reports');
+    	$menus_order = Array ( 'home','registrar','global','foundation','accounting','library','allreport','rsvacl','setting');
     	$temp_menu = Array();
     	$menus=array_unique($menus);
     	foreach ($menus_order as $i => $val){

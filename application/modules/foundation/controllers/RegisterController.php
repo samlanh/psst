a8081e5e3255debb634e6f87069a56f52b8dc3b2
samlanh <mok_channy@yahoo.com>
2017-07-11 11:43:44 +0700

    syheng commit new

--- application/modules/foundation/controllers/RegisterController.php
+++ application/modules/foundation/controllers/RegisterController.php
@@ -144,7 +144,7 @@ class Foundation_RegisterController extends Zend_Controller_Action {
 			$db = new Foundation_Model_DbTable_DbStudent();
 			$grade = $db->getAllGrade($data['dept_id']);
 			//print_r($grade);exit();
-			array_unshift($grade, array ( 'id' => 0, 'name' => 'Select Grade'));
+			array_unshift($grade, array ( 'id' => -1, 'name' => 'Add New'));
 			print_r(Zend_Json::encode($grade));
 			exit();
 		}

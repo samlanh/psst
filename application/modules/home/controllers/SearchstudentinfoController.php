816a83160079c863a9cf7e19703e8fa29de4b32a
samlanh <mok_channy@yahoo.com>
2017-09-26 12:48:37 +0700

    syheg commit new home

--- application/modules/home/controllers/SearchstudentinfoController.php
+++ application/modules/home/controllers/SearchstudentinfoController.php
@@ -7,7 +7,43 @@ class Home_SearchstudentinfoController extends Zend_Controller_Action {
     	header('content-type: text/html; charset=utf8');
     	defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
 	}
+	
 	public function indexAction(){
+		try{
+			if($this->getRequest()->isPost()){
+				$search=$this->getRequest()->getPost();
+			}
+			else{
+				$search = array(
+						'adv_search' => '',
+						'study_year'=> '',
+						'grade'=> '',
+						'session'=> '',
+						'time'=> '',
+						'degree'=> '',
+						'start_date'=> date('Y-m-d'),
+						'end_date'=>date('Y-m-d'));
+			}
+			$this->view->adv_search=$search;
+			$db_student= new Home_Model_DbTable_DbStudent();
+			$rs_rows = $db_student->getAllStudent($search);
+			$list = new Application_Form_Frmtable();
+			$collumns = array("BRANCH_NAME","STUDENT_ID","STUDENT_NAME","SEX","PHONE","ACADEMIC_YEAR","DEGREE","GRADE","SESSION","ROOM_NAME","STATUS");
+			$link=array(
+					'module'=>'home','controller'=>'searchstudentinfo','action'=>'student-detail',
+			);
+			$this->view->list=$list->getCheckList(0, $collumns, $rs_rows,array('branch_name'=>$link,'stu_code'=>$link,'name'=>$link,'stu_khname'=>$link,'grade'=>$link));
+		}catch (Exception $e){
+			Application_Form_FrmMessage::message("Application Error");
+			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+		}
+		$form=new Registrar_Form_FrmSearchInfor();
+		$form->FrmSearchRegister();
+		Application_Model_Decorator::removeAllDecorator($form);
+		$this->view->form_search=$form;
+	}
+	
+	public function studentDetailAction(){
 		$db= new Home_Model_DbTable_DbStudent();
 		try{
 			if($this->getRequest()->isPost()){
@@ -28,16 +64,17 @@ class Home_SearchstudentinfoController extends Zend_Controller_Action {
 						'start_date'=> date('Y-m-d'),
 						'end_date'=>date('Y-m-d'));
 			}
-			
+		    $id= $this->getRequest()->getParam('id');
+		    
 			$this->view->adv_search=$search;
-			$this->view->rs =$db->getStudentById($search);
-			$this->view->row = $db->getStudentPaymentDetail($search,1);
-			$this->view->service=$db->getStudentServiceUsing($search, 1);
-			
+			$this->view->rs =$db->getStudentById($id);
+			$rs=$this->view->row = $db->getStudentPaymentDetail($id);
+			$this->view->service=$db->getStudentServiceUsing($id,$search, 1);
+				
 		}catch (Exception $e){
 			Application_Form_FrmMessage::message("Application Error");
 			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
-		}	
+		}
 		$form=new Registrar_Form_FrmSearchInfor();
 		$form->FrmSearchRegister();
 		Application_Model_Decorator::removeAllDecorator($form);

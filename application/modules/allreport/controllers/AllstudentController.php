a2597005894469b3635a85ed3d6b98a40d3c4afc
samlanh <mok_channy@yahoo.com>
2017-12-27 14:15:12 +0700

    syheng commit new

--- application/modules/allreport/controllers/AllstudentController.php
+++ application/modules/allreport/controllers/AllstudentController.php
@@ -617,4 +617,29 @@ class Allreport_AllstudentController extends Zend_Controller_Action {
 		$this->view->search=$search;
 	}
 	
+	public function rptReschedulebygroupAction(){
+		if($this->getRequest()->isPost()){
+			$search=$this->getRequest()->getPost();
+		}
+		else{
+			$search=array(
+					'title' =>'',
+					'study_year' 	=>'',
+					'group' =>'',
+					'room' 	=>'',
+					'grade' =>'',
+					'session' =>'',
+					'start_date'=>date("Y-m-d"),
+					'end_date'=>date("Y-m-d")
+			);
+		}
+		$form=new Registrar_Form_FrmSearchInfor();
+		$forms=$form->FrmSearchRegister();
+		Application_Model_Decorator::removeAllDecorator($forms);
+		$this->view->form_search=$form;
+		$group= new Allreport_Model_DbTable_DbRptStudentDrop();
+		$this->view->rs = $rs_rows = $group->getAllReschedulebygroup($search);
+		$this->view->search=$search;
+	}
+	
 }
--- application/modules/allreport/models/DbTable/DbRptStudentDrop.php
+++ application/modules/allreport/models/DbTable/DbRptStudentDrop.php
@@ -108,5 +108,98 @@ class Allreport_Model_DbTable_DbRptStudentDrop extends Zend_Db_Table_Abstract
     	}
     	return $db->fetchAll($sql.$where.$order);
     }
+    //reschedule by group
+    function getAllReschedulebygroup($search=null){
+    	$db=$this->getAdapter();
+    	$sql="SELECT gr.id,gr.year_id,gr.group_id,gr.day_id,gr.from_hour,gr.to_hour,gr.subject_id,gr.techer_id,
+    	 
+    	(SELECT room_name AS NAME FROM `rms_room` WHERE is_active=1 AND room_name!='' AND rms_room.room_id=(SELECT g.room_id FROM rms_group AS g WHERE g.id=gr.group_id LIMIT 1) )AS room_name,
+    	(SELECT CONCAT(m.major_enname,' (',(SELECT d.en_name FROM rms_dept AS d WHERE m.dept_id=d.dept_id ),')')
+    	FROM rms_major AS m WHERE 1 AND major_enname!='' AND m.major_id=(SELECT g.grade FROM rms_group AS g WHERE g.id=gr.group_id LIMIT 1))AS grade_name,
+    	 
+    	REPLACE(CONCAT(gr.from_hour,'-',to_hour),' ','') AS times
+    	FROM rms_group_reschedule AS gr";
+    	 
+    	$where =' where 1';
+    	$from_date =(empty($search['start_date']))? '1': "gr.create_date >= '".$search['start_date']." 00:00:00'";
+    	$to_date = (empty($search['end_date']))? '1': "gr.create_date <= '".$search['end_date']." 23:59:59'";
+    	$where.= " AND ".$from_date." AND ".$to_date;
     
+    	if(!empty($search['title'])){
+    		$s_where = array();
+    		$s_search = addslashes(trim($search['title']));
+    		$s_where[] = " gr.`note` LIKE '%{$s_search}%'";
+    		$where .=' AND ('.implode(' OR ',$s_where).')';
+    	}
+    
+    	$order=" GROUP BY gr.year_id,gr.group_id
+    	ORDER BY gr.year_id,gr.group_id,times DESC ";
+    	 
+    	if(!empty($search['room'])){
+    		$where.=' AND (SELECT rms_group.room_id FROM rms_group WHERE rms_group.id=gr.group_id )='.$search['room'];
+    	}
+    	if(!empty($search['grade'])){
+    		$where.=' AND (SELECT rms_group.grade FROM rms_group WHERE rms_group.id=gr.group_id )='.$search['grade'];
+    	}
+    	if(!empty($search['study_year'])){
+    		$where.=' AND gr.year_id='.$search['study_year'];
+    	}
+    	if(!empty($search['group'])){
+    		$where.=' AND gr.group_id='.$search['group'];
+    	}
+    
+    	return $db->fetchAll($sql.$where.$order);
+    	 
+    }
+    
+    function getSubleByYGS($year,$group){
+    	$db=$this->getAdapter();
+    	$sql="SELECT gr.id,gr.year_id,gr.group_id,gr.day_id,gr.from_hour,gr.to_hour,gr.subject_id,gr.techer_id,
+    	REPLACE(CONCAT(gr.from_hour,'-',to_hour),' ','') AS times,
+    	(SELECT s.subject_titleen FROM rms_subject AS s WHERE s.id=gr.subject_id LIMIT 1) AS subject_name,
+    	(SELECT t.teacher_name_en FROM rms_teacher AS t WHERE t.id=gr.techer_id LIMIT 1) AS teacher_name,
+    	(SELECT t.tel FROM rms_teacher AS t WHERE t.id=gr.techer_id LIMIT 1) AS teacher_phone
+    	FROM rms_group_reschedule AS gr
+    	 
+    	WHERE gr.year_id=$year
+    	AND gr.group_id=$group
+    	 
+    	ORDER BY times ASC ";
+    	return $db->fetchAll($sql);
+    }
+    
+    function getSubleByHour($year,$group,$hour){
+    $db=$this->getAdapter();
+    $sql="SELECT gr.id,gr.year_id,gr.group_id,gr.day_id,gr.from_hour,gr.to_hour,gr.subject_id,gr.techer_id,
+    REPLACE(CONCAT(gr.from_hour,'-',to_hour),' ','') AS times,
+    (SELECT s.subject_titleen FROM rms_subject AS s WHERE s.id=gr.subject_id LIMIT 1) AS subject_name,
+    (SELECT t.teacher_name_en FROM rms_teacher AS t WHERE t.id=gr.techer_id LIMIT 1) AS teacher_name,
+    (SELECT t.tel FROM rms_teacher AS t WHERE t.id=gr.techer_id LIMIT 1) AS teacher_phone
+    FROM rms_group_reschedule AS gr
+    
+    WHERE gr.year_id=$year
+    AND gr.group_id=$group
+    
+    AND REPLACE(CONCAT(gr.from_hour,'-',gr.to_hour),' ','')='$hour'
+    GROUP BY gr.day_id
+    ORDER BY gr.day_id,times ASC ";
+    return $db->fetchAll($sql);
+    }
+    
+    function getSubjectListByYG($year,$group){
+    $db=$this->getAdapter();
+    $sql="SELECT gr.id,gr.year_id,gr.group_id,gr.day_id,gr.from_hour,gr.to_hour,gr.subject_id,gr.techer_id,
+    REPLACE(CONCAT(gr.from_hour,'-',to_hour),' ','') AS times,
+    (SELECT s.subject_titleen FROM rms_subject AS s WHERE s.id=gr.subject_id LIMIT 1) AS subject_name,
+    (SELECT t.teacher_name_en FROM rms_teacher AS t WHERE t.id=gr.techer_id LIMIT 1) AS teacher_name,
+    (SELECT t.tel FROM rms_teacher AS t WHERE t.id=gr.techer_id LIMIT 1) AS teacher_phone,
+    COUNT(*)AS total_hour
+    FROM rms_group_reschedule AS gr
+    WHERE gr.year_id=$year
+    AND gr.group_id=$group
+    GROUP BY gr.subject_id
+    ORDER BY subject_name,gr.subject_id DESC ";
+    return $db->fetchAll($sql);
+    }
+    //end reschedule by group
 }

add3088785f70e2534b4b54d76075a334bf30772
samlanh <mok_channy@yahoo.com>
2018-03-19 16:09:33 +0700

    move stock qty from register to stock report

--- application/modules/allreport/controllers/StockController.php
+++ application/modules/allreport/controllers/StockController.php
@@ -287,4 +287,35 @@ class Allreport_StockController extends Zend_Controller_Action {
 		Application_Model_Decorator::removeAllDecorator($form);
 		$this->view->form_search=$form;
 	}
+	public function rptproductqtysoldAction(){
+		try{
+			if($this->getRequest()->isPost()){
+				$search=$this->getRequest()->getPost();
+			}else{
+				$search=array(
+						'adv_search' 	=>'',
+						'start_date'	=>date("Y-m-d"),
+						'end_date'		=>date("Y-m-d"),
+						'user'			=>'',
+						'stu_code'		=>'',
+						'stu_name'		=>'',
+						'pro_name'		=>'',
+				);
+			}
+			$db = new Registrar_Model_DbTable_DbProductsold();
+			$this->view->rspro = $db->getProductSold($search);
+				
+			$this->view->all_pro = $db->getAllProductInProgramName($search);
+				
+			$form=new Registrar_Form_FrmSearchInfor();
+			$form->FrmSearchRegister();
+			Application_Model_Decorator::removeAllDecorator($form);
+			$this->view->form_search=$form;
+			$this->view->search = $search;
+		}catch(Exception $e){
+			Application_Form_FrmMessage::message("APPLICATION_ERROR");
+			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+			echo $e->getMessage();
+		}
+	}
 }

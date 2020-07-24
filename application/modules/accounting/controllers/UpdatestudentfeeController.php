c8bcfa679e7586d8a5551eaa29fe95428627df0b
samlanh <mok_channy@yahoo.com>
2020-07-24 11:07:27 +0700

    DD Update Fee Student

---
+++ application/modules/accounting/controllers/UpdatestudentfeeController.php
@@ -0,0 +1,101 @@
+
+<?php
+class Accounting_UpdatestudentfeeController extends Zend_Controller_Action {
+    public function init()
+    {    	
+    	$this->tr = Application_Form_FrmLanguages::getCurrentlanguage();
+    	header('content-type: text/html; charset=utf8');
+    	defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
+	}
+public function indexAction()
+    {
+    	try{
+    		if($this->getRequest()->isPost()){
+    			$search = $this->getRequest()->getPost();
+    			$this->view->row_ace=$search;
+    		}
+    		else{
+    			$search=array(
+	    				'title' 			=> '',
+	    				'academic_year' 	=> '',
+	    				'branch_id'			=>'',
+    					'type_study'		=>-1,
+    					'school_option'		=>-1,
+    					'is_finished_search' => '',
+    					'status' 			=>-1,
+    				);
+    		}
+    		$db = new Accounting_Model_DbTable_DbUpdateStudenFee();
+    		$rs_rows= $db->getAllTuitionFee($search);
+    		$list = new Application_Form_Frmtable();
+    		$collumns = array("BRANCH","ACADEMIC_YEAR","TYPE_STUDY","IS_MULTY_STUDY","TYPE","AMOUNT_STUDENT","School Option","CREATED_DATE","PROCESS_TYPE","BY_USER","STATUS");
+    		$link=array(
+    			'module'=>'accounting','controller'=>'updatestudentfee','action'=>'edit',
+    		);
+    		$this->view->list=$list->getCheckList(10, $collumns, $rs_rows , array());
+    	}catch (Exception $e){
+    		Application_Form_FrmMessage::message("APPLICATION_ERROR");
+    		Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+    	}    	
+    	$this->view->adv_search = $search;
+    	$_db = new Application_Model_DbTable_DbGlobal();
+    	$this->view->branch = $_db->getAllBranch();    	
+    	$frm = new Accounting_Form_FrmFee();
+    	$frm->FrmTutionfee();
+    	Application_Model_Decorator::removeAllDecorator($frm);
+    	$this->view->frm_fee = $frm;
+    	
+    	$form=new Registrar_Form_FrmSearchInfor();
+    	$form->FrmSearchRegister();
+    	Application_Model_Decorator::removeAllDecorator($form);
+    	$this->view->form_search=$form;
+    }
+    
+	function addAction(){
+		$db = new Accounting_Model_DbTable_DbUpdateStudenFee();
+		try{
+			if($this->getRequest()->isPost()){
+				$_data=$this->getRequest()->getPost();
+				$search = $_data;
+				$rs =$db->getSearchStudent($search);
+				$this->view->rs = $rs;
+			}else{
+				$search = array(
+						'branch_id' => '',
+						'degree' => '',
+						'grade' => '',
+						'session' => '',
+						'academic_year'=> ''
+					);
+			}
+			$this->view->search=$search;
+		}catch(Exception $e){
+			Application_Form_FrmMessage::message("APPLICATION_ERROR");
+			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+		}
+		$form=new Application_Form_FrmSearchGlobal();
+		$forms=$form->FrmSearch();
+		Application_Model_Decorator::removeAllDecorator($forms);
+		$this->view->form_search=$form;
+	}
+	public function submitAction(){
+		if($this->getRequest()->isPost()){
+			try{
+				$_data = $this->getRequest()->getPost();
+				
+				$db = new Accounting_Model_DbTable_DbUpdateStudenFee();
+				$db->addStudentGroup($_data);
+				Application_Form_FrmMessage::message("INSERT_SUCCESS");
+				if(isset($_data['save_close'])){
+					$this->_redirect('/accounting/updatestudentfee');
+				}else{
+					$this->_redirect('/accounting/updatestudentfee/add');
+				}
+			}catch(Exception $e){
+				Application_Form_FrmMessage::message("INSERT_FAIL");
+				Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+			}
+		}
+	}
+	
+}
---
+++ application/modules/accounting/models/DbTable/DbUpdateStudenFee.php
@@ -0,0 +1,160 @@
+<?php
+
+class Accounting_Model_DbTable_DbUpdateStudenFee extends Zend_Db_Table_Abstract
+{
+	
+	protected $_name = 'rms_group_detail_student';
+	public function getUserId(){
+		$session_user=new Zend_Session_Namespace(SYSTEM_SES);
+		return $session_user->user_id;
+	
+	}
+	function getAllTuitionFee($search=null){
+		$db=$this->getAdapter();
+		$dbp = new Application_Model_DbTable_DbGlobal();
+		 
+		$session_lang=new Zend_Session_Namespace('lang');
+		$lang = $session_lang->lang_id;
+		$field = 'name_en';
+		$str = 'title_eng';
+		if ($lang==1){
+			$field = 'name_kh';
+			$str = 'title_kh';
+		}
+		 
+		$sql = "SELECT t.id,
+		(SELECT CONCAT(branch_nameen) from rms_branch WHERE br_id =t.branch_id LIMIT 1) AS branch,
+		(SELECT CONCAT(fromYear,'-',toYear) FROM rms_academicyear WHERE rms_academicyear.id=t.academic_year LIMIT 1) as academic,
+		(SELECT $str FROM rms_studytype WHERE rms_studytype.id =t.term_study  LIMIT 1) AS study_type,
+		CASE is_multi_study
+		WHEN 1 THEN 'MULTY_PROGRAM'
+		WHEN 0 THEN 'ONE_PROGRAM_ONLY'
+		END is_multistudy,
+		t.generation,
+		(SELECT count(sfh.student_id) FROM rms_student_fee_history AS sfh WHERE sfh.fee_id = t.id LIMIT 1 ) AS amountStudent,
+		(SELECT title FROM `rms_schooloption` WHERE rms_schooloption.id=t.school_option LIMIT 1) as school_option,
+		t.create_date,
+		(SELECT $field from rms_view where type=12 and key_code=t.is_finished) as is_finished,
+		(SELECT CONCAT(first_name) from rms_users where rms_users.id = t.user_id) as user ";
+		 
+		$sql.=$dbp->caseStatusShowImage("t.status");
+		$sql.=" FROM `rms_tuitionfee` AS t
+		WHERE t.type=1	";
+		 
+		$where =" ";
+	
+		if(!empty($search['title'])){
+			$s_where = array();
+			$s_search = addslashes(trim($search['title']));
+			$s_where[] = " t.generation LIKE '%{$s_search}%'";
+			$where .=' AND ( '.implode(' OR ',$s_where).')';
+		}
+		$where.=" AND t.is_finished=0 ";
+		
+		if(!empty($search['academic_year'])){
+				$where.=" AND t.id=".$search['academic_year'];
+		}
+		if(!empty($search['branch_id'])){
+			$where.=" AND t.branch_id=".$search['branch_id'];
+		}
+		if($search['type_study']>0){
+			$where.=" AND t.term_study=".$search['type_study'];
+		}
+		if($search['school_option']>0){
+			$where.=" AND t.school_option=".$search['school_option'];
+		}
+		if($search['status']>-1){
+		$where.=" AND t.status=".$search['status'];
+		}
+		 
+		$where.=$dbp->getAccessPermission();
+		$order=" GROUP BY t.branch_id,t.academic_year,t.term_study,generation ORDER BY t.id DESC ";
+		 
+		return $db->fetchAll($sql.$where.$order);
+	}
+	
+	public function addStudentGroup($_data){
+		$db = $this->getAdapter();
+		try{
+			
+			if(!empty($_data['public-methods'])){
+				$_dbFee = new Accounting_Model_DbTable_DbFee();
+				$feeId = empty($_data['academic_year_fee'])?0:$_data['academic_year_fee'];
+				$row = $_dbFee->getFeeById($feeId);
+				
+				$all_stu_id = $_data['public-methods'];
+				
+				foreach ($all_stu_id as $stu_id){
+					
+					$this->_name = 'rms_student_fee_history';
+					$data_gro = array(
+							'is_current'=> 0,
+					);
+				
+					$where = 'student_id = '.$stu_id." AND is_current=1";
+					$this->update($data_gro, $where);
+					
+					$arr = array(
+								'user_id'	=>$this->getUserId(),
+								'branch_id'	=>$_data['branch'],
+								'student_id'	=>$stu_id,
+								'status'	=>1,
+								'academic_year'			=>$row['academic_year'],
+								'fee_id'				=>$feeId,
+								'is_current'		=>1,
+								'create_date'		=>date("Y-m-d H:i:s"),
+								'modify_date'		=>date("Y-m-d H:i:s"),
+						);
+					
+						$this->_name='rms_student_fee_history';
+						$this->insert($arr);
+						
+				}
+				
+			}
+		}catch(Exception $e){
+			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+		}
+	}
+	
+	function getSearchStudent($search){
+		$db=$this->getAdapter();
+		$sql="SELECT 
+				sd.gd_id,
+				s.stu_id,
+				s.stu_code,
+				s.stu_enname,
+				s.stu_khname,
+				s.last_name,
+				s.sex,
+				sd.degree,
+				sd.grade,
+				(SELECT sf.fee_id FROM `rms_student_fee_history` AS sf WHERE sf.student_id = s.stu_id AND sf.is_current = 1 ORDER BY sf.id DESC LIMIT 1) AS fee_id,
+				(SELECT tf.academic_year FROM `rms_tuitionfee` AS tf WHERE tf.id =(SELECT sf.fee_id FROM `rms_student_fee_history` AS sf WHERE sf.student_id = s.stu_id AND sf.is_current = 1 ORDER BY sf.id DESC LIMIT 1) LIMIT 1) AS academic_year,
+				(SELECT `title` FROM `rms_items` WHERE `id`=sd.degree AND TYPE=1 LIMIT 1) AS degree_title,
+				(SELECT CONCAT(`title`) FROM `rms_itemsdetail` WHERE `id`=sd.grade AND items_type=1 LIMIT 1) AS grade_title
+			  FROM 
+			  	rms_student AS s,
+			  	`rms_group_detail_student` AS sd 
+		 	  WHERE 
+				s.stu_id = sd.stu_id
+				AND s.`status`=1 
+				AND s.customer_type = 1 
+				AND sd.stop_type=0
+				AND sd.is_maingrade = 1 ";
+		if(!empty($search['academic_year'])){
+			$sql.=" AND (SELECT tf.academic_year FROM `rms_tuitionfee` AS tf WHERE tf.id =(SELECT sf.fee_id FROM `rms_student_fee_history` AS sf WHERE sf.student_id = s.stu_id AND sf.is_current = 1 ORDER BY sf.id DESC LIMIT 1) LIMIT 1) =".$search['academic_year'];
+		}
+		if(!empty($search['degree'])){
+			$sql.=" AND sd.degree =".$search['degree'];
+		}
+		if(!empty($search['branch_id'])){
+			$sql.=" AND s.branch_id =".$search['branch_id'];
+		}
+		$sql.=" GROUP BY s.stu_id,sd.degree,sd.grade";
+		$sql.=" ORDER BY s.stu_id DESC ";
+		return $db->fetchAll($sql);
+	}
+
+}
+
